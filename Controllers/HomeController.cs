155e34aab97ea595de82cfb367a2ab52ebe02886
jenny ann justimbaste <145590422+Justimbaste@users.noreply.github.com>
2023-09-23 16:14:21 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+           new StudentInfoModel { Name = "Jenny Ann Justimbaste", StudentId = "S001", StudentAge = "20" },
+            new StudentInfoModel { Name = "Cloe Sabrina Villanueva", StudentId = "S002", StudentAge = "22" },
+            new StudentInfoModel { Name = "Vien Paul Villanueva", StudentId = "S003", StudentAge = "31" }
         };
 
         return View(studentInfoArray);
